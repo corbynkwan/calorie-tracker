24fce5fc03b3dc5e7c7ad2bd6361246e785e80ac
Amy <amy34268@gamil.com>
2022-06-10 16:52:13 -0700

    Added InputForm Component and CustomFood Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Restaurant  from './Restaurant';
 function App() {
   return (
     <div className="App">
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Restaurant/>
         <a
           className="App-link"
           href="https://reactjs.org"
