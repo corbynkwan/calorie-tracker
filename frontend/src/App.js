e9874d9eb27ee63df695ac1ae4ef7237b2c07534
Dhruv <26849655+dhruv-tech@users.noreply.github.com>
2022-06-11 08:32:38 -0700

    fixes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,20 +3,14 @@ import './App.css';
 import RestaurantCard from "./Restaurant/Restaurant";
 import {Button, ButtonBase, Container, Grid, Paper, styled, Typography} from "@material-ui/core";
 import ItemSelectionComponent, {ItemCard} from "./Item/Item";
+import Header from './Header';
 
 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-
-          <Button variant="contained" size="large">
-              Hello Wolrd
-          </Button>
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-        <RestaurantCard></RestaurantCard>
+      <Header/>
     </div>
 
   );
