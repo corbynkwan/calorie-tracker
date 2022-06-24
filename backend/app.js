cfff0b20084bace88751d3f6ced8310d53a977cf
Dhruv <26849655+dhruv-tech@users.noreply.github.com>
2022-06-23 20:37:22 -0700

    init

---
+++ backend/app.js
@@ -0,0 +1,62 @@
+/*
+    Entrypoint - Backend
+*/
+
+const express = require('express');
+const db = require('./db/db.config');
+
+const app = express();
+
+// Importing Services
+
+const test = require('./services/test');
+
+// Middleware
+
+app.use(express.json());
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Headers', 'accept, authorization, content-type, x-requested-with');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    next();
+});
+
+
+app.get('/test', async(req, res) => {
+
+    const retrivedData = await test.getAll();
+    res.statusCode = retrivedData.code;
+
+    res.json(retrivedData);
+
+})
+
+app.post('/test', async(req, res) => {
+    
+    const response = await test.add(req.body);
+    res.statusCode = response.code;
+    res.json(response);
+
+})
+
+const port = 6000 || process.env.port;
+app.listen(port, async() => {
+    
+    try {
+        console.log(`\x1b[36mStarting API...\x1b[0m`);
+
+        console.log(`\x1b[33m→ Connecting to Database...\x1b[0m`)
+        await db.connect();
+        console.log(`\x1b[32m → Connected.\x1b[0m`)
+        console.log(`\x1b[36mCalorie Tracker API listening on port ${port}\x1b[0m`)
+
+    } catch (e) {
+        console.log(`\x1b[31m → Connection Refused.\n Please see error below: \n \x1b[0m`)
+        console.error(e)
+        console.log(`Calorie Tracker API could not start.`)
+    }
+
+    
+})
---
+++ backend/db/db.config.js
@@ -0,0 +1,57 @@
+/*
+    Database Setup
+*/
+
+require('dotenv').config();
+const mongoose = require("mongoose");
+
+const db = {};  
+
+// Establish new connection with MongoDB Instance
+
+db.connect = async() => {
+
+    return new Promise(async(resolve, reject) => {
+
+        try {
+
+            await mongoose.connect(process.env.MONGODB_SRV);
+            resolve();
+
+
+        } catch (e) {
+
+            console.error(e);
+            reject();
+        }
+
+    });
+}
+
+
+// Schemas & Models
+
+const testSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    testedAt: {
+        type: Date,
+        default: () => Date.now()
+    },
+    reason: {
+        type: String,
+        required: false
+    }
+});
+
+db.Test = mongoose.model("Test", testSchema);
+
+/*
+    TODO: Add Schemas & Models for all data types here.
+*/
+
+// Export
+
+module.exports = db;
