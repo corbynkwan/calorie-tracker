cf0c18f079634be905fb7d815fbe8b52d8076a16
Zehao Lu <92357274+ZehaoLu01@users.noreply.github.com>
2022-08-06 21:38:25 -0700

    save work

--- frontend/src/components/Recommended/Recommended.jsx
+++ frontend/src/components/Recommended/Recommended.jsx
@@ -1,18 +1,31 @@
 import { Button, Grid} from '@mui/material';
 import './Recommended.css';
-import { useSelector, useDispatch } from 'react-redux'
+import { useSelector, useDispatch, setState} from 'react-redux'
 import { useEffect } from 'react'
 import { Food } from '../Foods/Food';
 import { getAllFoods } from '../../store/foodsSlice';
+
+async function getRecommendation() {
+
+}
+
 export default function Recommended() {
     const dispatch = useDispatch();
+    const [recommendation,setRecommendation] = useState([]);
+    let recommended = sets
     useEffect(() => {
-        dispatch(getAllFoods())
-    }, [dispatch]); 
+       console.log("recommend rendered");
+       getRecommendation();
+    }, []);
     const foods = useSelector(state => state.foods.allFoods);
     const renderFoods = () => {
         let res = [];
         for (let i = 0; i < foods.length; i++) {
+            //      name: "Burger",
+            //       calories: 750,
+            //       filters: [],
+            //       from: "Triple O's",
+            //       thumbnail: "https://food.ubc.ca/wp-content/uploads/2020/01/Triple_B-1024x684.jpg"
           res.push(
             <Grid item xs={3}> <Food food={foods[i]}></Food></Grid>
           );
