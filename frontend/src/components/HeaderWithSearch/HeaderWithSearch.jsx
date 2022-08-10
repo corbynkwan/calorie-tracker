7c4fefe95b2565971374fb797dbc075fcd259098
Dhruv <26849655+dhruv-tech@users.noreply.github.com>
2022-08-09 22:54:52 -0700

    search bar

--- frontend/src/components/HeaderWithSearch/HeaderWithSearch.jsx
+++ frontend/src/components/HeaderWithSearch/HeaderWithSearch.jsx
@@ -3,11 +3,17 @@ import { Button } from "@mui/material";
 import { Link } from "react-router-dom";
 import { useSelector } from 'react-redux';
 import logo from "../../assets/images/alpha-logo.svg";
+import { InstantSearch, SearchBox } from 'react-instantsearch-dom';
+import { searchClient } from "../../typesenseAdapter";
 
 export default function HeaderWithSearch(props) {
   return (
     <section className="SearchHeader">
-     <img src={logo} alt="Alpha Calorie Tracker Logo"></img>
+      <img src={logo} alt="Alpha Calorie Tracker Logo"></img>
+
+      <InstantSearch searchClient={searchClient}>
+        <SearchBox></SearchBox>
+      </InstantSearch>
     </section>
   );
 }
